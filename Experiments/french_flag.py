42a0baa5f00eebc1444813f1522bf79a925e1889
sammyhansali <sammyhansali@gmail.com>
2022-11-11 10:46:16 -0500

    agents changes

--- Experiments/french_flag.py
+++ Experiments/french_flag.py
@@ -26,13 +26,17 @@ goal = [[ 1, 1, 1, 3, 3, 3, 2, 2, 2],
         [ 1, 1, 1, 3, 3, 3, 2, 2, 2]]
 
 
-ANN_inputs=     [       "m0_to_send", 
-                        "GJ_opening_molecs", 
-                        "stress_to_send", 
-                        "GJ_opening_stress", 
-                        "anxio_to_send", 
+ANN_inputs=     [       "molecules", 
+                        "energy", 
+                        "energyt1", 
+                        "stress", 
+                        "stresst1", 
+                        "state",
+                        "statet1",
+                        "local_geometrical_frustration",
                         "collective_size", 
                         "french_flag",
+                        "bias",
                 ] 
 ANN_outputs=    [       "m0_to_send", 
                         "GJ_opening_molecs", 
@@ -45,10 +49,4 @@ ANN_outputs=    [       "m0_to_send",
 exp = experiment(start, goal, ANN_inputs, ANN_outputs)
 exp.nb_gens=1
                         
-# exp.ANN_outputs.extend(["apoptosis", 
-#                         "cell_division",
-#                         ])
-# exp.nb_ANN_inputs = len(exp.ANN_inputs)-1
-# exp.nb_ANN_outputs = len(exp.ANN_outputs)
-# print(exp.ANN_outputs)
 run_experiment(exp)
