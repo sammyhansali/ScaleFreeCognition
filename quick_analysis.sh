f3a1ade307504a37f3acfaf0b48ee2c162dd2b35
sammyhansali <sammyhansali@gmail.com>
2023-02-24 20:56:28 -0500

    removed file from remote

--- quick_analysis.sh
+++
@@ -1,8 +0,0 @@
-file=$(find /cluster/tufts/levinlab/shansa01/ScaleFreeCognition/SCS_Results/ -name "$1*")
-echo $file
-cd $file
-
-module load anaconda/2021.11
-source activate mesamultineat
-python analysis.py
-
