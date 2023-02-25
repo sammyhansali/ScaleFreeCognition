6a17f78b8dbd7235ffb9d2c911b5d4935ac62de6
sammyhansali <sammyhansali@gmail.com>
2023-02-24 20:06:45 -0500

    removing files

--- SCS_Results/2023-02-20_08:47:54_100/run_analysis.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import MultiNEAT as NEAT
-from analysis import sim
-
-with open("exp.pickle", "rb") as fp:
-    exp = pickle.load(fp)
-best_genome = NEAT.Genome("best_genome.txt")
-
-sim(exp, best_genome)
--- activate_conda.sh
+++
@@ -1,3 +0,0 @@
-#!/cluster/home/shansa01/
-module load anaconda/2021.11
-source activate mesamultineat
