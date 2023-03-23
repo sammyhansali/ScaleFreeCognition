404c1ac05ad8f92a3a464a9ad529e793273b0850
sammyhansali <sammyhansali@gmail.com>
2023-03-23 12:01:15 -0400

    tests

--- extra_generalizability.py
+++ extra_generalizability.py
@@ -9,10 +9,38 @@ import sys
 
 ### JUST HAVE TO BE ABLE TO EXTRA GENERALIZE ONCE - SINCE THERE IS TOO MUCH ERROR/VARIANCE
 
-file_names='''
-2023/Mar/13/22:25:14_96
+# file_names='''
+# 2023/Mar/13/22:25:14_96
+# '''.split()
+file_names = '''
+2023/Mar/20/19:58:46_98
+2023/Mar/21/03:40:20_97
+2023/Mar/20/16:10:47_97
+2023/Mar/20/17:26:42_96
+2023/Mar/21/07:18:43_95
+2023/Mar/20/11:42:28_97
+2023/Mar/20/21:57:16_96
+2023/Mar/20/16:46:15_91
+2023/Mar/20/14:17:42_88
+2023/Mar/21/04:22:24_88
+2023/Mar/20/19:35:58_91
+2023/Mar/20/08:00:36_88
+2023/Mar/20/23:56:08_87
+2023/Mar/21/05:31:51_86
+2023/Mar/21/01:19:04_85
+2023/Mar/20/09:33:35_95
+2023/Mar/21/01:28:58_90
+2023/Mar/21/09:57:31_89
+2023/Mar/20/12:49:47_88
+2023/Mar/21/06:26:29_83
+2023/Mar/21/01:24:39_93
+2023/Mar/20/17:19:47_88
+2023/Mar/20/15:27:19_88
+2023/Mar/20/23:24:22_87
+2023/Mar/21/15:23:29_85
 '''.split()
 
+all_scores = []
 
 def getOptions():
     new_stims = [
@@ -182,3 +210,18 @@ for file_name in file_names:
             extra_generalizable+=1
         print("~~~~~~~~~~~")
     print(f"{file_name} has a EXTRA generalizability score of {extra_generalizable} out of 5!", flush=True)
+    all_scores.append(extra_generalizable)
+
+import statistics
+mode_1_1 = all_scores[:5]
+mode_1_2 = all_scores[5:10]
+mode_1_3 = all_scores[10:15]
+mode_1_4 = all_scores[15:20]
+mode_1_5 = all_scores[20:25]
+modes=[mode_1_1, mode_1_2, mode_1_3, mode_1_4, mode_1_5]
+for mode in modes:
+    avg = statistics.mean(mode)
+    st = statistics.stdev(mode)
+    mi = min(mode)
+    ma = max(mode)
+    print(f"This mode has an average robust score of {avg}, with standard deviation of {st}, min of {mi}, and max of {ma}")
