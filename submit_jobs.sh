2f0f9f9f3b32d3a12f46eabec755cdc330b43a54
sammyhansali <sammyhansali@gmail.com>
2023-09-27 11:51:20 -0400

    script update

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -5,7 +5,7 @@ FILE_PATH=$1
 CORES=$2
 FILE_NAME=$(basename "$FILE_PATH" .py)
 CURRENT_DATE=$(date +"%Y/%b/%d")
-mkdir -p /cluster/tufts/levinlab/shansa01/SFC/jobs_logs/$CURRENT_DATE
+mkdir -p /cluster/tufts/levinlab/shansa01/projects/SFC/jobs_logs/$CURRENT_DATE
 
 sbatch << EOT
 #!/bin/sh
@@ -15,15 +15,15 @@ sbatch << EOT
 #SBATCH -N 1
 #SBATCH -n "$CORES"
 #SBATCH --mem=32g
-#SBATCH --output="/cluster/tufts/levinlab/shansa01/SFC/jobs_logs/$CURRENT_DATE/${FILE_NAME}.out"
-#SBATCH --error="/cluster/tufts/levinlab/shansa01/SFC/jobs_logs/$CURRENT_DATE/${FILE_NAME}.err"
+#SBATCH --output="/cluster/tufts/levinlab/shansa01/projects/SFC/jobs_logs/$CURRENT_DATE/${FILE_NAME}.out"
+#SBATCH --error="/cluster/tufts/levinlab/shansa01/projects/SFC/jobs_logs/$CURRENT_DATE/${FILE_NAME}.err"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sh1436@scarletmail.rutgers.edu
 
 ## commands to run
 module load anaconda/2021.11
 source activate mesamultineat
-cd /cluster/tufts/levinlab/shansa01/SFC/ScaleFreeCognition
+cd /cluster/tufts/levinlab/shansa01/projects/SFC/ScaleFreeCognition
 
 ## Test 1
 # No pos, No fit inputs
